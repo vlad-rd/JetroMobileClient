913ab0ae00ced76793ba2890a867ceb5112308ed
ranhaveshushwork <ran.haveshush.work@gmail.com>
2014-12-14 12:37:17 +0200

    ConnectionsListActivity: improve the ConnectionsAdapter with ViewHolder design pattern;

--- src/com/jetro/mobileclient/ui/activities/ConnectionsListActivity.java
+++ src/com/jetro/mobileclient/ui/activities/ConnectionsListActivity.java
@@ -10,12 +10,13 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.View.OnLongClickListener;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -67,6 +68,23 @@ public class ConnectionsListActivity extends HeaderActivity {
 			}
 		});
 		mConnectionsList = (ListView) mBaseContentLayout.findViewById(R.id.connections_list);
+		mConnectionsList.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				Connection connection = mConnections.get(position);
+				Intent intent = new Intent(ConnectionsListActivity.this, LoginActivity.class);
+				intent.putExtra(Config.Extras.EXTRA_CONNECTION, connection);
+				startActivity(intent);
+			}
+		});
+		mConnectionsList.setOnItemLongClickListener(new OnItemLongClickListener() {
+			@Override
+			public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+				Connection connection = mConnections.get(position);
+				launchConnectionActionsDialog(connection);
+				return true;
+			}
+		});
 	}
 	
 	@Override
@@ -133,107 +151,94 @@ public class ConnectionsListActivity extends HeaderActivity {
 
 		@Override
 		public View getView(final int position, View convertView, ViewGroup parent) {
-
+			ViewHolder holder;
+			
+			if (convertView == null) {
+				holder = new ViewHolder();
+				convertView = mInflater.inflate(R.layout.list_item_connection, parent, false);
+				holder.connectionName = (TextView) convertView.findViewById(R.id.connection_name);
+				holder.deleteButton = (Button) convertView.findViewById(R.id.delete_button);
+				convertView.setTag(holder);
+			} else {
+				holder = (ViewHolder) convertView.getTag();
+			}
+			
 			final Connection connection = mConnections.get(position);
-			
 			String connectionName = connection.getName();
-			
-			convertView = mInflater.inflate(R.layout.list_item_connection, parent, false);
-			View listItem = convertView.findViewById(R.id.list_item);
-			TextView hostName = (TextView) convertView.findViewById(R.id.connection_name);
-			View deleteButton = convertView.findViewById(R.id.delete_button);
-			
-
-			listItem.setOnClickListener(
-					new OnClickListener() {
-						@Override
-						public void onClick(View v) {
-							Intent intent = new Intent(ConnectionsListActivity.this, LoginActivity.class);
-							intent.putExtra(Config.Extras.EXTRA_CONNECTION, connection);
-							startActivity(intent);
-						}
-					});
-
-			listItem.setOnLongClickListener(
-					new OnLongClickListener() {
-						@Override
-						public boolean onLongClick(View v) {
-							launchConnectionActionsDialog(connection);
-							return true;
-						}
-					});
-			
-			hostName.setText(connectionName);
-
-			deleteButton.setOnClickListener(
-					new OnClickListener() {
-						@Override
-						public void onClick(View v) {
-							launchDeleteConnectionDialog(connection, position);
-						}
-					});
+			holder.connectionName.setText(connectionName);
+			holder.deleteButton.setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					launchDeleteConnectionDialog(connection, position);
+				}
+			});
 			
 			return convertView;
 		}
 		
-		private void launchConnectionActionsDialog(final Connection host) {
-			Log.d(TAG, TAG + "#launchConnectionActionsDialog(...) ENTER");
-
-			final Dialog dialog = new Dialog(ConnectionsListActivity.this);
-			dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-			dialog.setContentView(R.layout.dialog_connection_actions_list);
-			ListView connectionActionsList = (ListView) dialog.findViewById(R.id.connection_actions_list);
-			connectionActionsList.setOnItemClickListener(new OnItemClickListener() {
-				@Override
-				public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-					String[] connectionActionsOptions = getResources().getStringArray(R.array.connection_actions_options);
-					String connectionActionOption = ((TextView) view).getText().toString();
-
-					// Connection option connect
-					if (connectionActionsOptions[0].equals(connectionActionOption)) {
-						Intent intent = new Intent(ConnectionsListActivity.this, LoginActivity.class);
-						intent.putExtra(Config.Extras.EXTRA_CONNECTION, host);
-						startActivity(intent);
-					// Connection option details
-					} else if (connectionActionsOptions[1].equals(connectionActionOption)) {
-						Intent intent = new Intent(ConnectionsListActivity.this, ConnectionActivity.class);
-						intent.putExtra(Config.Extras.EXTRA_CONNECTION_ACTIVITY_STATE, ConnectionActivity.State.VIEW_CONNECTION);
-						intent.putExtra(Config.Extras.EXTRA_CONNECTION, host);
-						startActivity(intent);
-					// Connection option delete
-					} else if (connectionActionsOptions[2].equals(connectionActionOption)) {
-						launchDeleteConnectionDialog(host, position);
-					}
-					dialog.dismiss();
-				}
-			});
-			dialog.show();
+		private class ViewHolder {
+			TextView connectionName;
+			Button deleteButton;
 		}
 		
-		private void launchDeleteConnectionDialog(final Connection host, final int position) {
-			Log.d(TAG, TAG + "#launchDeleteConnectionDialog(...) ENTER");
-			
-			DialogLauncher.launchDeleteConnectionDialog(
-					ConnectionsListActivity.this,
-					new DialogInterface.OnClickListener() {
-						public void onClick(DialogInterface dialog, int which) {
-							if (which == DialogInterface.BUTTON_POSITIVE) {
-								mConnectionsDB.deleteConnection(host.getName());
-								mConnections.remove(position);
-								if (mConnectionsDB.isDBEmpty()) {
-									Intent intent = new Intent(ConnectionsListActivity.this, ConnectionActivity.class);
-									intent.putExtra(Config.Extras.EXTRA_CONNECTION_ACTIVITY_STATE, ConnectionActivity.State.ADD_CONNECTION);
-									startActivity(intent);
-									dialog.dismiss();
-									finish();
-								} else {
-									notifyDataSetChanged();
-								}
+	}
+	
+	private void launchConnectionActionsDialog(final Connection connection) {
+		Log.d(TAG, TAG + "#launchConnectionActionsDialog(...) ENTER");
+
+		final Dialog dialog = new Dialog(ConnectionsListActivity.this);
+		dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		dialog.setContentView(R.layout.dialog_connection_actions_list);
+		ListView connectionActionsList = (ListView) dialog.findViewById(R.id.connection_actions_list);
+		connectionActionsList.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				String[] connectionActionsOptions = getResources().getStringArray(R.array.connection_actions_options);
+				String connectionActionOption = ((TextView) view).getText().toString();
+
+				// Connection option connect
+				if (connectionActionsOptions[0].equals(connectionActionOption)) {
+					Intent intent = new Intent(ConnectionsListActivity.this, LoginActivity.class);
+					intent.putExtra(Config.Extras.EXTRA_CONNECTION, connection);
+					startActivity(intent);
+				// Connection option details
+				} else if (connectionActionsOptions[1].equals(connectionActionOption)) {
+					Intent intent = new Intent(ConnectionsListActivity.this, ConnectionActivity.class);
+					intent.putExtra(Config.Extras.EXTRA_CONNECTION_ACTIVITY_STATE, ConnectionActivity.State.VIEW_CONNECTION);
+					intent.putExtra(Config.Extras.EXTRA_CONNECTION, connection);
+					startActivity(intent);
+				// Connection option delete
+				} else if (connectionActionsOptions[2].equals(connectionActionOption)) {
+					launchDeleteConnectionDialog(connection, position);
+				}
+				dialog.dismiss();
+			}
+		});
+		dialog.show();
+	}
+	
+	private void launchDeleteConnectionDialog(final Connection host, final int position) {
+		Log.d(TAG, TAG + "#launchDeleteConnectionDialog(...) ENTER");
+		
+		DialogLauncher.launchDeleteConnectionDialog(
+				ConnectionsListActivity.this,
+				new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int which) {
+						if (which == DialogInterface.BUTTON_POSITIVE) {
+							mConnectionsDB.deleteConnection(host.getName());
+							mConnections.remove(position);
+							if (mConnectionsDB.isDBEmpty()) {
+								Intent intent = new Intent(ConnectionsListActivity.this, ConnectionActivity.class);
+								intent.putExtra(Config.Extras.EXTRA_CONNECTION_ACTIVITY_STATE, ConnectionActivity.State.ADD_CONNECTION);
+								startActivity(intent);
+								dialog.dismiss();
+								finish();
+							} else {
+								mConnectionsAdapter.notifyDataSetChanged();
 							}
 						}
-					});
-		}
-		
+					}
+				});
 	}
 	
 }
